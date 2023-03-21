534c56205bf3cfc5c01e8aae69867cbc744df536
JackPan <jheng.wei0919@gmail.com>
2023-03-21 14:58:24 +0800

    add db

--- app/src/main/java/com/jackpan/travelnote_android/ui/dbhelper/RoomHelper.kt
+++ app/src/main/java/com/jackpan/travelnote_android/ui/dbhelper/RoomHelper.kt
@@ -1,11 +1,22 @@
 package com.jackpan.travelnote_android.ui.dbhelper
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Entity
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [(UserInfo::class)], version = 1)
 abstract class  RoomHelper  : RoomDatabase(){
     abstract fun userDao(): UserDao
+    companion object{
+        private  var instance : RoomHelper? = null
+        private  var DB_NAME = ""
+        fun getInstance(context: Context ) : RoomHelper{
+            return  instance ?: Room.databaseBuilder(context,RoomHelper::class.java, DB_NAME)
+                .fallbackToDestructiveMigration()
+                .build().also { instance = it }
+        }
+    }
 
 }
